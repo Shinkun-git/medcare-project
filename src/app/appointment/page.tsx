9446ccbb7cb9fd662f6ff336e425b351fecda509
prsnt <prshntksinku7@gmail.com>
2025-03-16 18:48:19 +0530

    footer css relative

--- src/app/appointment/page.tsx
+++ src/app/appointment/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Footer from "../components/layouts/Footer/Footer";
 import Navbar from "../components/layouts/Navbar/Navbar";
 import styles from "./page.module.css"
@@ -12,14 +11,14 @@ const appointmentPage = () => {
     return (
         <>
             <Navbar />
-            <main className={styles.container}>
+            {/* <main className={styles.container}>
                 <section className={`${styles.searchField} ${MontserratFont.className}`}>
                     <span>
                         Find a doctor at your own ease
                     </span>
                     <SearchDoctor/>
                 </section>
-            </main>
+            </main> */}
             <Footer />
         </>
     );
