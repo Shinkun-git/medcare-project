ff4e4bbe715b405a83377f7563a52d60f151ec2d
prsnt <prshntksinku7@gmail.com>
2025-03-21 14:50:34 +0530

    pure sel. in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={styles.body}>
+    <html lang="en" id="html">
+      <body className={styles.body} id="body">
         {children}
       </body>
     </html>
