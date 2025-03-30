1b990047364bade1c033b5443f69b27cf860f4ec
prsnt <prshntksinku7@gmail.com>
2025-03-31 00:49:57 +0530

    nav sidebar done

--- src/app/components/layouts/Navbar/Navbar.tsx
+++ src/app/components/layouts/Navbar/Navbar.tsx
@@ -9,15 +9,24 @@ const MontserratFont = Montserrat({
     weight: "500"
 })
 
-
 export default function Navbar() {
     return (
         <nav className={`${styles.container} ${MontserratFont.className}`}>
+            <input type="checkbox" id="toggle" className={styles.toggleInput} />
+            <label htmlFor="toggle" className={styles.menuBtn}>
+                <img src="/menu-white.svg" height={32} width={32} alt="menu" />
+            </label>
+
+            <div className={styles.linkContainer}>
+                <label htmlFor="toggle" className={styles.closeBtn}>
+                    <img src="/cross.svg" height={32} width={32} alt="close" />
+                </label>
                 <div className={styles.HeadLink}>
                     <NavHead />
                     <NavLinks />
                 </div>
                 <NavButtons />
+            </div>
         </nav>
     )
 } 
